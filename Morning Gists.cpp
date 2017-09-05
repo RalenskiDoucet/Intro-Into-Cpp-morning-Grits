9ba7186a09e68ccccbd2d84d11e91870a8a24cfe
RalenskiDoucet <Ralenskid@gmail.com>
2017-09-05 10:00:50 -0500

    commit to changes

---
+++ Morning Gists.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	system("pause");
+}
+
+// 1. Find the sum of the to biggest numbers in the array.
+int valueNumber1 = 15;
+int valueNumber2 = 75;
+int valueNumber3 = 45;
+int sumOfnumbers = 135;
+int avgOfnumbers = 45;
+int myArray[5][?];
+//2. The user may switch the values.
+// The user may change the the array.
