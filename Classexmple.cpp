9b56759d2ae90355d4c099a4e557eb077bbc8d00
RalenskiDoucet <Ralenskid@gmail.com>
2017-09-05 17:45:22 -0500

    i commit

---
+++ Classexmple.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+//class example#1.
+int addarrayValue(int myArray[],int legnth)
+{
+	int sum = 0;
+	for (int 1
+}
+
+
+
+//Class example #2.
+
+//add the sum of 2 numbers.
+int addTonumbers (int a, int b)
+{int result a+b;
+return result;
+}
+
+
+
+
+//subtract three numbers.
+int subNumbers(int h, int i, int j)
+{
+	int result = (h-i-j=)
+	return result;
+}
+
+
+
+
+//mutiple any number by 10.
+int mutipleby10 (int )
+
+
+
+
+//halves any value.
--- Morning Gists.cpp
+++ Morning Gists.cpp
@@ -5,12 +5,21 @@ int main()
 	system("pause");
 }
 
+int arrayFunction(int myArray[], int arrayLenght)
+{
+	return 135;
+}
+
 // 1. Find the sum of the to biggest numbers in the array.
-int valueNumber1 = 15;
-int valueNumber2 = 75;
-int valueNumber3 = 45;
-int sumOfnumbers = 135;
-int avgOfnumbers = 45;
-int myArray[5][?];
+
+int valueNumber1 =15;
+int valueNumber2 =75;
+int valueNumber3 =45;
+int sumOfnumbers =135;
+
+
+	
+
+
 //2. The user may switch the values.
 // The user may change the the array.
